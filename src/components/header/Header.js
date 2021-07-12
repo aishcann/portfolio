80e797a090402de5b6f3959c53b6a42c4b063d77
aishcann <aish.cann@gmail.com>
2021-07-12 07:50:52 -0400

    basic mvp

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,39 +1,44 @@
-import React from 'react';
-import './header.css'
+import './header.scss';
+import data from '../../links.json';
 
 const Header = () => {
-
-    return (
-			<div className='header'>
-				<section className='intro'>
-					<h1> Hi there! </h1>
-					<img src='prof photo.jpeg' alt='Aisha' className='prof-photo'/>
-					<p>
-						I’m a creative problem solver and clear technical communicator
-						actively looking for roles within web development / software
-						engineering! I have brought my passion and talent into so many
-						different sectors, from tech to education to performance art. Driven
-						by a mix of intuition and hard won study and application, I strive
-						to deliver work that is aesthetically pleasing, intuitive, and
-						accessible, with high levels of functionality.
-					</p>
-				    <h3>my tech stack</h3>
-                    <ul>
-                        <li>HTML / CSS</li>
-                        <li>Javascript</li>
-                        <li>React</li>
-                        <li>Python</li>
-                        <li>Django</li>
-                        <br />
-                        <li>PostgreSQL</li>
-                        <li>Node</li>
-                        <li>MongoDB</li>
-                        <li>Sass</li>
-                        <li>Git / Github</li>
-                    </ul>
-				</section>
-			</div>
-		);
+	return (
+		<div className='header'>
+			<section className='intro'>
+				<h1> Hi there! </h1>
+				<img src='assets/prof photo.jpeg' alt='Aisha' className='prof-photo' />
+				<div className='links'>
+					{data.map((d) => (
+						<a href={d.url} target='_blank' rel='noreferrer'>
+							<img src={d.icon} alt='' />
+						</a>
+					))}
+				</div>
+				<p>
+					I’m a creative problem solver and clear technical communicator
+					actively looking for roles within web development / software
+					engineering! I have brought my passion and talent into so many
+					different sectors, from tech to education to performance art. Driven
+					by a mix of intuition and hard won study and application, I strive to
+					deliver work that is aesthetically pleasing, intuitive, and
+					accessible, with high levels of functionality.
+				</p>
+				<h3>my tech stack</h3>
+				<ul>
+					<li>HTML / CSS</li>
+					<li>Javascript</li>
+					<li>React</li>
+					<li>Python</li>
+					<li>Django</li>
+					<li>PostgreSQL</li>
+					<li>Node</li>
+					<li>MongoDB</li>
+					<li>Sass</li>
+					<li>Git / Github</li>
+				</ul>
+			</section>
+		</div>
+	);
 };
 
 export default Header;
--- src/components/portfolio/Porfolio.js
+++ src/components/portfolio/Porfolio.js
@@ -1,68 +1,45 @@
-import { useState } from 'react'
-import './portfolio.css'
+import './portfolio.scss';
 
 const Porfolio = () => {
 
 
     return (
-			<div className='porfolio'>
-				<h2>my work</h2>
-				<section className='project-buttons'>
-					<div className='button'>
-						<h3>In Your City</h3>
-						<p>
-							Full Stack app utilizing React, Python, Django, PostgreSQL, and
-							Sass
-						</p>
-					</div>
-					<div className='button'>
-						<h3>The Future is Clearer</h3>
-						<p>A React app using a third party API and CSS</p>
-					</div>
-					<div className='button'>
-						<h3>On Pointe</h3>
-						<p>A trivia game created using HTML, CSS, and Javascript</p>
-					</div>
-				</section>
-				<section className='project-details'>
-					<div className='each-project'>
-						<div>
-							<img
-								src='in-your-city.png'
-								alt='In Your City'
-								className='project-image'
-							/>
-						</div>
-						<div>
-							<h4>In Your City</h4>
-						</div>
-						<div>
-							<p>Technologies: React, Sass, Python, Django, PostgreSQL</p>
-						</div>
-						<div className='links'>
-							<a href='https://in-your-city.herokuapp.com/'>
-								<p>Deployed Site</p>
-							</a>
-							<a href='https://github.com/aishcann/in-your-city-frontend'>
-								<p>Github</p>
-							</a>
-						</div>
-					</div>
-					{/* <div className='project'>
-						<img
-							src='the-future-is-clearer.png'
-							alt='The Future is Clearer'
-							className='project-image'
-						/>
-					</div>
-					<div className='project'>
-						<img
-							src='on-pointe.png'
-							alt='On Pointe'
-							className='project-image'
-						/>
-					</div> */}
-				</section>
+			<div>
+				<div className='porfolio'>
+					<h2>my work</h2>
+					<section className='project-buttons'>
+						<a
+							href='https://in-your-city.herokuapp.com/'
+							target='_blank'
+							rel='noreferrer'>
+							<div className='button'>
+								<h3>In Your City</h3>
+								<p>
+									Full Stack app utilizing React, Python, Django, PostgreSQL,
+									and Sass
+								</p>
+							</div>
+						</a>
+						<a
+							href='http://the-future-is-clearer.herokuapp.com/'
+							target='_blank'
+							rel='noreferrer'>
+							<div className='button'>
+								<h3>The Future is Clearer</h3>
+								<p>A React app using a third party API and CSS</p>
+							</div>
+						</a>
+						<a
+							href='https://aishcann.github.io/on-pointe/'
+							target='_blank'
+							rel='noreferrer'>
+							<div className='button'>
+								<h3>On Pointe</h3>
+								<p>A trivia game created using HTML, CSS, and Javascript</p>
+							</div>
+						</a>
+					</section>
+				</div>
 			</div>
 		);
 };
